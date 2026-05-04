7b7c62a8361e1f4c3afa7cd2ae1b455512374ebe
RyanAiden-DK <henrybaperzo@gmail.com>
2026-05-05 00:35:45 +0800

    feat: implement core image processing logic: add_watermark

--- processor.py
+++ processor.py
@@ -1,6 +1,5 @@
 import sys
-
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import os
 
 # 功能1：格式转换-尺寸不变压缩图片
@@ -11,7 +10,7 @@ def handle_image_mode(img):
         background = Image.new("RGB", img.size, (255, 255, 255))
         if img.mode == "P":
             img = img.convert("RGBA")
-        # 粘贴时使用 img 自身作为蒙版（mask）
+        # 粘贴时使用 img 自身作为蒙版
         background.paste(img, (0, 0), img)
         return background
     elif img.mode != 'RGB':
@@ -52,3 +51,62 @@ def resize_image(input_path, width_ratio, height_ratio, output_path):
     except Exception as e:
         print(f"error: {e}")
         return None
+
+
+#功能3：水印
+def add_watermark(input_path, output_path, text, font_size, transparency, position):
+    try:
+        with Image.open(input_path).convert("RGBA") as base:
+            text_layer = Image.new("RGBA", base.size, (0,0,0,0))
+            # 加载字体
+            try:
+                font_rb = ImageFont.truetype("arial.ttf", font_size)
+                font_d = ImageFont.truetype("arial.ttf", int(font_size*3))
+            # 默认字体可能不支持中文
+            except IOError:
+                font_rb = ImageFont.load_default()
+                font_d = ImageFont.load_default()
+
+            draw = ImageDraw.Draw(text_layer)
+
+            if position in ["right_bottom", "both"]:
+                #测量文字尺寸
+                bbox = draw.textbbox((0,0),text,font=font_rb)
+                text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+                # 留白防止超出
+                x = base.width-text_width-15
+                y = base.height-text_height-40
+
+                draw.text((x, y), text, font=font_rb, fill=(0,0,0,transparency))
+
+
+            if position in ["diagonal", "both"]:
+                # 同样先测量文字大小
+                bbox_d = draw.textbbox((0, 0), text, font=font_d)
+                tw, th = bbox_d[2] - bbox_d[0], bbox_d[3] - bbox_d[1]
+
+                # 创建一个小画布，大小刚好够放字
+                # 留白防止旋转时边缘切割                                        黑色水印用透明黑
+                txt_img = Image.new("RGBA", (tw + 10, th + 10), (0, 0, 0, 0))
+                d = ImageDraw.Draw(txt_img)
+                d.text((5, 5), text, font=font_d, fill=(0, 0, 0, transparency))
+
+                # 旋转画布向右上方倾斜
+                # expand=True 会自动适应文字旋转后大小                      BICUBIC适用于文字旋转
+                rotated_txt = txt_img.rotate(45, expand=True, resample=Image.BICUBIC)
+
+                # 计算中心点，把旋转后的水印贴到 text_layer 中心
+                # 使用 rotated_txt 自身作为蒙版
+                cx = (base.width - rotated_txt.width) // 2
+                cy = (base.height - rotated_txt.height) // 2
+                text_layer.paste(rotated_txt, (cx, cy), rotated_txt)
+
+            # 透明合成比paste好
+            out = Image.alpha_composite(base, text_layer)
+
+            # 转回 RGB 保存（JPEG不支持透明度）
+            out.convert("RGB").save(output_path, "JPEG")
+            print(f"水印添加成功: {output_path}")
+
+    except Exception as e:
+        print(f"添加水印失败: {e}")
